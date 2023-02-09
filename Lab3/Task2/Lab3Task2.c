ce0fb157a31822a4f2911dea061c31477c29562b
Krister Emren <criz_81@yahoo.com>
2023-02-09 19:24:28 +0100

    Task 2 not fully done

--- Lab3/Task2/Lab3Task2.c
+++ Lab3/Task2/Lab3Task2.c
@@ -21,6 +21,7 @@ AUTOSTART_PROCESSES(&node_process);
 PROCESS_THREAD(node_process, ev, data)
 {
   int is_coordinator;
+  int r_count;
 
   PROCESS_BEGIN();
 
@@ -40,6 +41,7 @@ PROCESS_THREAD(node_process, ev, data)
     while(1) {
         LOG_INFO("Routing entries: %u\n", uip_ds6_route_num_routes());
         uip_ds6_route_t *route = uip_ds6_route_head();
+        r_count = 0;
       	while(route) {
 		LOG_INFO("Route ");
 		LOG_INFO_6ADDR(&route->ipaddr);
@@ -47,7 +49,28 @@ PROCESS_THREAD(node_process, ev, data)
 		LOG_INFO_6ADDR(uip_ds6_route_nexthop(route));
 		LOG_INFO_("\n");
 		route = uip_ds6_route_next(route);
+    r_count++;
 	}
+  leds_off(LEDS_ALL);
+  // assume the root node does not care about being alone
+  if(node_id > 1) {
+    if(r_count > 0) {
+      // we are an intermediate node
+      LOG_INFO("Intermediate %d\n", LEDS_BLUE);
+      leds_off(LEDS_GREEN | LEDS_RED);
+      leds_on(64);
+    } else {
+      // we are end node
+      LOG_INFO("End node %d\n", LEDS_RED);
+      leds_off(LEDS_GREEN | 64);
+      leds_on(LEDS_RED);
+    }
+  } else {
+    // we are root node
+    LOG_INFO("Root node %d\n", LEDS_GREEN);
+    leds_off(LEDS_RED | 64);
+    leds_on(LEDS_GREEN);
+  }
       	PROCESS_YIELD_UNTIL(etimer_expired(&et));
       	etimer_reset(&et);
     }
